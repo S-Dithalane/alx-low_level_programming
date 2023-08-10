72b72b7ea02dfa5ca991b87011b34c86f5957094
Solomon Dithalane <dithalane@gmail.com>
2023-08-10 04:45:05 -0700

    C program that use puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	puts("Programming is like a building a multilingual puzzle\n");
+	return (0);
+}
