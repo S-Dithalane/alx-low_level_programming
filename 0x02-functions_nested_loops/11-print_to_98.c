daf9c46e7761d942bb1660d1b57a7bb59f36ba3a
Solomon Dithalane <dithalane@gmail.com>
2023-08-16 19:26:35 -0700

    Print all natural numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Prints numbers to 98
+ * @n: Number to be printed from
+ * Followed by a new line
+ */
+void print_to_98(int n)
+{
+	srand(time(NULL));
+
+	int n = rand() % 10 + 1;
+
+	while (n <= 98)
+	{
+		_putchar(n);
+		if (n < 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+		n++
+	}
+}
