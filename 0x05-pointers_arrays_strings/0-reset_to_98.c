ad0852a8459a4036ff20bd93baca5c4edc1104c7
Solomon Dithalane <dithalane@gmail.com>
2023-08-21 03:12:28 -0700

    Changing the value of the variable using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Change the value of the variable to 98
+ * @n: Pointer to the parameter to be changed
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
