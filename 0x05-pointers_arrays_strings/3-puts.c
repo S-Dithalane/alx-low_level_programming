b4c47d7f83f9a562a0a1e53738678ad765b99770
Solomon Dithalane <dithalane@gmail.com>
2023-08-21 05:54:51 -0700

    Print the string in reverse

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints the string in reverse order
+ * @s: Pointer that points to the string
+ * Followed by a new line to stdout
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	int i =count - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
