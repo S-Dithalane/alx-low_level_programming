afaf7c07a786d2eb419cbcc0c54217bd2746352a
Solomon Dithalane <dithalane@gmail.com>
2023-08-24 10:07:42 -0700

    Copying a string element

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy a string
+ * @dest: String destination
+ * @src: The string to be copied
+ * @n: The parameter to be checked
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
