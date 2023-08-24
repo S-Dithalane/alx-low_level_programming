448a807eabcc5fbbfb427941e4a8b93782e3ae01
Solomon Dithalane <dithalane@gmail.com>
2023-08-24 10:25:48 -0700

    Comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,16 +4,18 @@
  * _strcmp - Compares two strings
  * @s1: String to be compared to s2
  * @s2: String to be compared to s1
+ * Return: The difference in string length
  */
-int _strcmp(char *s1, char s2)
+int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	int i = 0;
+
+	while (s1[i] == s2[i])
 	{
-		if (*s1 != *s2)
-		{
-			return (*s1 - *s2);
-		}
-		*s1++;
-		*s2++;
+		if (s1[i] == '\0')
+			return (s1[i] - s2[i]);
+		i++;
 	}
+
+	return (s1[i] - s2[i]);
 }
