5eaf8149e7ac7d14534fec96a535183de59c931a
Solomon Dithalane <dithalane@gmail.com>
2023-08-24 18:53:50 -0700

    Capitalize all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @z: Pointer pointing at a string
+ * Return: Capitalized string
+ */
+char *cap_string(char *z)
+{
+	int i = 0;
+	int j;
+	char symbols[] = " \n\t,;.!?\"(){}";
+
+
+	if (z[0] >= 'a' && z[0] <= 'z')
+		z[0] -= 32;
+
+	for (; z[i] != '\0'; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			if (z[i] == symbols[j])
+			{
+				if (z[i + 1] >= 'a' && z[i + 1] <= 'z')
+					z[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (z);
+}
